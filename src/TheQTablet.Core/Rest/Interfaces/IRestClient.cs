5b0e8ec3eddceffad10f80c1b1beabd9b6fcf0b2
Valeria Plutino <valeria.plutino@uts.edu.au>
2020-09-11 14:14:56 +1000

    Added RestClient files

--- src/TheQTablet.Core/Rest/Interfaces/IRestClient.cs
+++ src/TheQTablet.Core/Rest/Interfaces/IRestClient.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
 namespace TheQTablet.Core.Rest.Interfaces
 {
-    public class IRestClient
+    public interface IRestClient
     {
-        public IRestClient()
-        {
-        }
+        Task<TResult> MakeApiCallAsync<TResult>(string url, HttpMethod method, object data = null)
+                      where TResult : class;
     }
 }
