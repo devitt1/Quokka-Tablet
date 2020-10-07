fd70bca018987c00a392d1032c0d5c85b07f5d6f
Valeria Plutino <valeria.plutino@uts.edu.au>
2020-10-07 11:01:53 +1100

    Changed location of OperationType

--- src/TheQTablet.Core/DataModel/QsimAPIResults.cs
+++ src/TheQTablet.Core/DataModel/QsimAPIResults.cs
@@ -28,17 +28,5 @@ namespace TheQTablet.Core.DataModel
     {
         public string Status { get; set; } = "";
     }
-
-    public enum OperationType
-    {
-        CREATE_CIRCUIT,
-        GATE,
-        SET_STATE,
-        DESTROY_CIRCUIT,
-        MEASURE,
-        MEASURE_QUBITS,
-        STATE_VECTOR,
-        QSAM
-    }
     
 }
