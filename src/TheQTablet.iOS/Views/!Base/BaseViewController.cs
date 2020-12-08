66f4f048a8196ec92c8f38e15fe18af36f206e8c
Tim Porritt <timothy.m.porritt@student.uts.edu.au>
2020-12-08 17:53:19 +1100

    WIP Customised navbar

--- src/TheQTablet.iOS/Views/!Base/BaseViewController.cs
+++ src/TheQTablet.iOS/Views/!Base/BaseViewController.cs
@@ -6,6 +6,46 @@ using UIKit;
 
 namespace TheQTablet.iOS
 {
+    public class ExitLessonButton : UIView
+    {
+        private UIImageView _logo;
+        private UILabel _text;
+
+        public ExitLessonButton()
+        {
+            _logo = new UIImageView
+            {
+                TranslatesAutoresizingMaskIntoConstraints = false,
+                ContentMode = UIViewContentMode.ScaleAspectFit,
+                Image = UIImage.FromBundle("back_logo"),
+            };
+            AddSubview(_logo);
+
+            _text = new UILabel
+            {
+                TranslatesAutoresizingMaskIntoConstraints = false,
+                Text = "EXIT LESSON 1",
+                TextColor = ColorPalette.PrimaryText,
+                Font = FontGenerator.GenerateFont(20, UIFontWeight.Regular),
+            };
+            AddSubview(_text);
+
+            _logo.LeftAnchor.ConstraintEqualTo(LeftAnchor).Active = true;
+            _text.RightAnchor.ConstraintEqualTo(RightAnchor).Active = true;
+
+            _logo.RightAnchor.ConstraintEqualTo(_text.LeftAnchor, -14).Active = true;
+            _logo.WidthAnchor.ConstraintEqualTo(_logo.HeightAnchor).Active = true;
+
+            TopAnchor.ConstraintLessThanOrEqualTo(_logo.TopAnchor).Active = true;
+            TopAnchor.ConstraintLessThanOrEqualTo(_text.TopAnchor).Active = true;
+            BottomAnchor.ConstraintGreaterThanOrEqualTo(_logo.BottomAnchor).Active = true;
+            BottomAnchor.ConstraintGreaterThanOrEqualTo(_text.BottomAnchor).Active = true;
+
+            _text.CenterYAnchor.ConstraintEqualTo(CenterYAnchor).Active = true;
+            _logo.CenterYAnchor.ConstraintEqualTo(CenterYAnchor).Active = true;
+        }
+    }
+
     public abstract class BaseViewController<TViewModel> : MvxViewController<TViewModel>
         where TViewModel : class, IMvxViewModel
     {
@@ -21,6 +61,15 @@ namespace TheQTablet.iOS
             NavigationController.NavigationBar.BarTintColor = ColorPalette.Primary;
             NavigationController.NavigationBar.TintColor = UIColor.White;
             NavigationController.NavigationBar.SetBackgroundImage(UIImage.FromBundle("banner_gradient"), UIBarMetrics.Default);
+
+            NavigationItem.HidesBackButton = true;
+            NavigationItem.LeftBarButtonItem = new UIBarButtonItem(new ExitLessonButton());
+            NavigationItem.TitleView = new UILabel
+            {
+                Text = "LESSON 1 : LIGHT POLARISATION",
+                TextColor = ColorPalette.PrimaryText,
+                Font = FontGenerator.GenerateFont(22, UIFontWeight.Regular),
+            };
 
             NavigationController.SetNeedsStatusBarAppearanceUpdate();
 
